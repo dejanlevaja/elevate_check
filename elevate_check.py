31c20fdc2fca83ee1d00d24a23637d6cce604b3b
dejanlevaja <dejan@levaja.com>
2017-02-11 15:10:20 +0100

    Recursive file listing fix. Manifest extraction using pefile instead of BS

--- elevate_check.py
+++ elevate_check.py
@@ -1,44 +1,75 @@
-import glob
+import os
 import platform
-import re
 import sys
+import pefile
 import argparse
 from tabulate import tabulate
 from BeautifulSoup import BeautifulSoup as BS
 from BeautifulSoup import Comment
+from fnmatch import fnmatch
 
+
+# READ https://www.greyhathacker.net/?p=796
 __author__ = 'Dejan Levaja'
 __email__ = 'dejan[@]levaja.com'
 __license__ = 'GPLv2'
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 
 ver = platform.platform()
 table = []
 
+def list_all_files(root, pattern, recursive):
+    allfiles = []
+    for path, subdirs, files in os.walk(root):
+        for name in files:
+            if fnmatch(name, pattern):
+                fpath = os.path.join(path, name)
+                allfiles.append(fpath)
+        if not recursive:
+            return allfiles
+            break
 
-def get_data(fname, ignore_ms):
-    with open(fname, "rb") as f:
-        raw = f.read()
-    unicode_str = re.compile(u'[\u0020-\u007e]{3,}', re.UNICODE)
-    strings = unicode_str.findall(raw)
-    soup = BS('\n'.join(strings))
-    elevator = soup.find('autoelevate')
-    if elevator:
-        el= elevator.string
-        if str(el) == 'true':
-            desc = soup.find('description')
-            if desc:
-                description = desc.string
-            else:
-                description = ''
-            comments = soup.findAll(text=lambda text: isinstance(text, Comment))
-            for comment in comments:
-                if "Copyright" in comment:
-                    manufacturer = comment.strip()
-                    if 'microsoft' in manufacturer.lower():
-                        if not ignore_ms:
-                            text = "%s,%s,%s" % (fname, description, manufacturer)
-                            table.append(text.split(','))
+    return allfiles
+
+def get_data(manifest, fname, ignore_ms):
+    # print '\nFILENAME: %s' % fname
+    # print 'MANIFEST: """%s"""' % manifest
+
+    if manifest:
+        soup = BS(manifest)
+        elevator = soup.find('autoelevate')
+        if elevator:
+            el= elevator.string
+            if str(el) == 'true':
+                desc = soup.find('description')
+                if desc:
+                    description = desc.string
+                else:
+                    description = ''
+                comments = soup.findAll(text=lambda text: isinstance(text, Comment))
+                for comment in comments:
+                    if "Copyright" in comment:
+                        manufacturer = comment.strip()
+                        if 'microsoft' in manufacturer.lower():
+                            if not ignore_ms:
+                                text = "%s,%s,%s" % (fname, description, manufacturer)
+                                table.append(text.split(','))
+
+def get_manifest(fname):
+    pe = pefile.PE(fname)
+    if hasattr(pe, 'DIRECTORY_ENTRY_RESOURCE'):
+        for resource_type in pe.DIRECTORY_ENTRY_RESOURCE.entries:
+            name = "%s" % pefile.RESOURCE_TYPE.get(resource_type.struct.Id)
+            if name and hasattr(resource_type, 'directory'):
+                for resource_id in resource_type.directory.entries:
+                    if hasattr(resource_id, 'directory'):
+                        for resource_lang in resource_id.directory.entries:
+                            manifest = pe.get_data(
+                                resource_lang.data.struct.OffsetToData,
+                                resource_lang.data.struct.Size)
+                            if 'MANIFEST' in name:
+                                return manifest
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -53,21 +84,20 @@ if __name__ == '__main__':
 
     print '\nPlease wait, it can take some time...'
 
-    if recursive:
-        base_path = directory + '\\*.exe'
-        filenames = glob.glob(base_path)
-        sub_path = directory + '\\**\\*.exe'
-        filenames += glob.glob(sub_path)
-    else:
-        base_path = directory + '\\*.exe'
-        filenames = glob.glob(base_path)
 
+    filenames = list_all_files(directory, "*.exe", recursive)
+
+    #filenames = ['C:\Windows\System32\wusa.exe']
     total = len(filenames)
+
 
     # progress
     for i, filename in enumerate(filenames):
         print '[%s of %s] Processing file "%s"' % (i+1, total, filename)
-        get_data(filename, ignore_ms)
+        manifest = get_manifest(filename)
+        get_data(manifest, filename, ignore_ms)
+
+
 
 
     # print table
